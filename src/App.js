a5bace6fba2d6a1816df0a7ba4939dcfe6ee16e9
zhouxk <1844423979@qq.com>
2021-12-02 19:50:40 +0800

    comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Input, Switch } from 'antd';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  state = {
+    data: '',
+    open: false
+  }
+
+  handChange = (e) => {
+    let curValue = e.target.value
+    this.setState({ data: curValue })
+    console.log(e)
+  }
+
+
+
+  handClick = () => {
+    const { data } = this.state
+    console.table(data.split(' '))
+  }
+
+  handOpen = (val) => {
+    console.log(val)
+  }
+
+  render () {
+    const { data, open } = this.state
+    return (
+
+      <div className="App">
+        <Switch onChange={this.handOpen} ></Switch><br/>
+        <Input.TextArea cols={2} rows={10} value={data.split('\n').map((v, i) => 10 * (i + 1)).join('\n')}></Input.TextArea>
+        <Input.TextArea cols={10} rows={10} value={data} onChange={this.handChange} ></Input.TextArea>
+        <Input.TextArea cols={2} rows={10} value={data.split('\n').map((v, i) => i + 1).join('\n')}></Input.TextArea><br />
+        <Button type='primary' onClick={this.handClick}>sumit</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
