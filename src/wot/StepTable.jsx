a01577f5a9e66d1875cbb6d772115897c8e7d2ab
zhouxk <zhouxk1024@163.com>
2022-01-26 16:25:49 +0800

    more

--- src/wot/StepTable.jsx
+++ src/wot/StepTable.jsx
@@ -1,39 +1,87 @@
-import React, { useState } from "react";
-import { Table, Divider, Modal, Input, Button } from "antd";
+import React, { useState, useRef } from "react";
+import { Table, Divider, Modal, Input, Tooltip, Popover, Popconfirm, message, Form } from "antd";
+import { PlusOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
+// 初始化测试数据
 const data = [
     {
         key: '1',
-        stepNo: '1',
-        stepDesc: '西湖区湖底公园1号',
+        stepNo: 1,
+        stepDesc: '西湖区湖底公园11111111111111111111111111111号',
     }, {
         key: '2',
-        stepNo: '2',
-        stepDesc: '西湖区湖底公园2号',
+        stepNo: 2,
+        stepDesc: '西湖区湖底公园22号',
     }, {
         key: '3',
-        stepNo: '2',
-        stepDesc: '西湖区湖底公园3号',
+        stepNo: 2,
+        stepDesc: '西湖区湖底公园22号',
     }, {
         key: '4',
-        stepNo: '3',
-        stepDesc: '西湖区湖底公园4号',
+        stepNo: 3,
+        stepDesc: '西湖区湖底公园33号',
     }, {
         key: '5',
-        stepNo: '4',
-        stepDesc: '西湖区湖底公园5号',
+        stepNo: 4,
+        stepDesc: '西湖区湖底公园44号',
+    }, {
+        key: '6',
+        stepNo: 4,
+        stepDesc: '西湖区湖底公园44号',
+    }, {
+        key: '7',
+        stepNo: 5,
+        stepDesc: '西湖区湖底公园55号',
     },
 ];
 
 function StepTable() {
+
     const [dataSource, setDataSource] = useState(data);
 
+    /**
+     * 新增步骤事件
+     * @param {*} index 新增上一行索引 
+     */
     const handleInsert = (index) => {
         window.event.preventDefault();
         setVisible(true)
         setCurIndex(index)
     }
 
+    /**
+     * 删除提示框确认事件
+     * @param {*} index 删除索引 
+     */
+    const handleDelete = (index) => {
+        setDataSource(
+            function () {
+                // 相同步序行删除，步序不更新
+                if (dataSource[index].stepNo === dataSource[index + 1]?.stepNo || dataSource[index].stepNo === dataSource[index - 1]?.stepNo) {
+                    return [...dataSource.slice(0, index), ...dataSource.slice(index + 1)]
+                }
+                // 更新后续步骤步序
+                return [
+                    ...dataSource.slice(0, index),
+                    ...dataSource.slice(index + 1).map(item => ({
+                        key: item.key,
+                        stepNo: --item.stepNo,
+                        stepDesc: item.stepDesc,
+                    }))
+                ]
+            }
+        )
+        message.success('Delete Success');
+    }
+
+    /**
+     * 步骤更新事件
+     * @param {*} index 更新索引
+     */
+    const handleUpdate = (index) => { }
+
+
+    // 表格抬头信息
     const columns = [
         {
             title: '序号',
@@ -44,6 +92,8 @@ function StepTable() {
             title: '步骤描述',
             dataIndex: 'stepDesc',
             key: 'stepDesc',
+            editable: true,
+            render: (text, record, index) => <Popover content={text}>{text.length >= 10 ? text.slice(0, 10) + '...' : text}</Popover>
         }, {
             title: '步序',
             dataIndex: 'stepNo',
@@ -54,73 +104,101 @@ function StepTable() {
             key: 'action',
             render: (text, record, index) => {
                 return <>
-                    <Button onClick={() => handleInsert(index)}>该行后插入</Button>
-                    {/* <Divider type="vertical" />
-                    <Button>更新步序</Button>
+                    <Tooltip placement="topLeft" title='下方增加步骤'><PlusOutlined onClick={() => handleInsert(index)} /></Tooltip>
                     <Divider type="vertical" />
-                    <Button>删除</Button> */}
+                    <Tooltip placement="topLeft" title='修改步序描述'><EditOutlined onClick={() => { handleUpdate(index) }} /></Tooltip>
+                    <Divider type="vertical" />
+                    <Popconfirm
+                        title="Are you sure to delete this task?"
+                        onConfirm={() => handleDelete(index)}
+                        okText="Yes"
+                        cancelText="No"
+                    >
+                        <Tooltip placement="topLeft" title='删除当前步序'><DeleteOutlined /></Tooltip>
+                    </Popconfirm>
                 </>
             }
         }
     ];
 
+    // 当前操作索引值
     const [curIndex, setCurIndex] = useState(-1)
-    const [addStep, setAddStep] = useState('')
 
-    const handleOk = () => {
-
+    // 计算序列
+    const calcData = (desc) => {
+        // 当前行与下一行步序一致
         if (dataSource[curIndex].stepNo === dataSource[curIndex + 1]?.stepNo) {
-            const addArr = addStep.split('\n').map(val => ({
+            const addArr = desc.split('\n').map(val => ({
                 key: val,
                 stepNo: dataSource[curIndex].stepNo,
                 stepDesc: val
             }))
             setDataSource([...dataSource.slice(0, curIndex + 1), ...addArr, ...dataSource.slice(curIndex + 1)])
         } else {
-            const addArr = addStep.split('\n').map((val, index) => ({
+            // 当前行与下一行步序不一致
+            const addArr = desc.split('\n').map((val, index) => ({
                 key: val,
                 stepNo: +dataSource[curIndex].stepNo + index + 1,
                 stepDesc: val
             }))
-            let num = 1
-            const tails = dataSource.slice(curIndex + 1).map((item, index, tail) => ({
+            const tails = dataSource.slice(curIndex + 1).map(item => ({
                 key: item.key,
                 stepDesc: item.stepDesc,
-                stepNo: (function () {
-                    if (index === 0) {
-                        return +addArr.at(-1).stepNo + 1
-                    }
-                    if (tail[index].stepNo === tail[index - 1].stepNo) {
-                        return +tail[index - 1].stepNo + num
-                    }
-                    num++
-                    return +tail[index - 1].stepNo + num
-                })(index),
+                stepNo: +item.stepNo + addArr.length
             }))
             setDataSource([...dataSource.slice(0, curIndex + 1), ...addArr, ...tails])
         }
         setVisible(false)
+        message.success('Insert Success');
     }
 
-    const handleCancel = () => { setVisible(false) }
-    const [visible, setVisible] = useState(false)
+    /**
+     * 新增步骤弹框确认事件
+     */
+    const handleOk = () => {
+        form.validateFields()
+            .then(data => {
+                calcData(data.stepDesc)
+            })
+            .catch(err => {
+                console.log('err', err)
+            })
+    }
 
+    const [visible, setVisible] = useState(false)
+    // 新增步骤弹框取消事件
+    const handleCancel = () => { setVisible(false) }
+    const [form] = Form.useForm()
+    const textEl = useRef(null);
     return (
-        <div style={{ width: '600px' }}>
+        <div style={{ width: '500px' }}>
             <Table
                 dataSource={dataSource}
                 columns={columns}
                 pagination={false}
             />;
             <Modal
-                title="新增步序"
+                title="新增步骤"
                 visible={visible}
                 onOk={handleOk}
                 onCancel={handleCancel}
             >
-                <Input.TextArea rows={6} value={addStep} onChange={e => setAddStep(e.target.value)}></Input.TextArea>
+                <Form
+                    form={form}
+                    ref={textEl}
+                >
+                    <Form.Item
+                        name="stepDesc"
+                        rules={[{ required: true, message: 'Please input your step description!' }]}
+                    >
+                        <Input.TextArea
+                            rows={6}
+                            placeholder="请输入步骤描述"
+                        />
+                    </Form.Item>
+                </Form>
             </Modal>
-        </div>
+        </div >
     )
 
 }
